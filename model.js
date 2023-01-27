7df5318fa0008e7ed28a1d2b89e1639ef517cabc
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-27 15:01:52 +0100

    Ferdiggjort "skew" effekt når man scroller opp og ned

--- model.js
+++ model.js
@@ -9,8 +9,38 @@ const model = {
 
   pictures: [
     {
-      id: "",
-      img: "",
+      id: 01,
+      img: "IMG/Picture1.jpg",
+      title: "",
+      description: "",
+    },
+    {
+      id: 02,
+      img: "IMG/Picture2.jpg",
+      title: "",
+      description: "",
+    },
+    {
+      id: 03,
+      img: "IMG/Picture3.jpg",
+      title: "",
+      description: "",
+    },
+    {
+      id: 04,
+      img: "IMG/Picture4.jpg",
+      title: "",
+      description: "",
+    },
+    {
+      id: 05,
+      img: "IMG/Picture5.jpg",
+      title: "",
+      description: "",
+    },
+    {
+      id: 06,
+      img: "IMG/Picture6.jpg",
       title: "",
       description: "",
     },
