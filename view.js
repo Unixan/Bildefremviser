1e400ad29a2b98423c0cfb4376ac502a44a97714
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-27 20:32:54 +0100

    Working. Ish....

--- view.js
+++ view.js
@@ -5,26 +5,26 @@ function updateView() {
 //mainView
 
 function updateMainView() {
-    let html = "";
-    html += `<section>`;
-    model.pictures.forEach((pic, i) => {
-        html += /*HTML*/ `
+  let html = "";
+  html += `<section>`;
+  model.pictures.forEach((pic, i) => {
+    html += /*HTML*/ `
         <div>
         <img src="${pic.img}"/>
         </div>
         `;
-    });
-    html += `</section>`;
-    document.getElementById("app").innerHTML = html;
-    const section = document.querySelector("section");
-    let currentPos = window.scrollY;
-    const update = () => {
-      const newPos = window.scrollY;
-      const diff = newPos - currentPos;
-      const speed = diff * 0.5;
-      section.style.transform = `skewY(${speed}deg)`;
-      currentPos = newPos;
-      requestAnimationFrame(update);
-    };
-    update();
+  });
+  html += `</section>`;
+  document.getElementById("app").innerHTML = html;
+  const section = document.querySelector("section");
+  let currentPos = window.scrollY;
+  const update = () => {
+    const newPos = window.scrollY;
+    const diff = newPos - currentPos;
+    const speed = diff * 0.5;
+    section.style.transform = `skewY(${speed}deg)`;
+    currentPos = newPos;
+    requestAnimationFrame(update);
+  };
+  update();
 }
